8656c85f954e152f4e8a0a66226412e08ddb6e36
Jonhoa <jonitoadriano@gmail.com>
2023-04-03 23:09:58 -0500

    Added seed data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-p = Person.create(first_name: "Malcolm", last_name: "Wheezy",email: "test@test.com", phone: 214242343, title: "Database Engineer", github: "github.com", linkedin: "linkedin", location: "NY")
+p = Person.create(
+  first_name: 'Malcolm',
+  last_name: 'Wheezy',
+  email: 'test@test.com',
+  phone: 214_242_343,
+  title: 'Database Engineer',
+  github: 'github.com',
+  linkedin: 'linkedin',
+  location: 'NY'
+)
 p.save
+
+e = Education.create(
+  start: 'May 2013',
+  end: 'June 2017',
+  degree: 'BSC Computer Science',
+  university: 'Skylar University',
+  details: 'Computer science course'
+)
+e.save
+
+x = Experience.create(
+  start: 'January 2021',
+  end: 'September 2023',
+  job: 'Forklift Driver',
+  company: 'Buso',
+  details: 'Worked in a warehouse and drove a forklift through the building'
+)
+x.save
+
+p = Project.create(
+  "name": 'Vasco Simulator',
+  "description": 'Experience the world in Vasco eyes, written in C++, and React'
+)
+p.save
+
+s = Skill.create(
+  name: 'C++'
+)
+s.save
